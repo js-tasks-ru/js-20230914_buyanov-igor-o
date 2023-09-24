db0b09d354cbaa23081da7ed976fca7378179ce3
Igor Buyanov <i.buyanov@mts.ai>
2023-09-24 22:36:40 +0300

    added tasks for homework

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -5,4 +5,19 @@
  */
 export function invertObj(obj) {
 
+    if (typeof(obj) == "undefined")
+    {
+        return undefined;
+    }
+    
+    const newObj = {};
+
+    for(let key in obj)
+    {
+        newObj[obj[key]] = key
+    }
+    return newObj
+
 }
+
+console.log(invertObj())
