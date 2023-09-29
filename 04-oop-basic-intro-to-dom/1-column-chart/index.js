8a5536529c1dd2ee4a8506fb84a65b6f41ad8f10
Igor Buyanov <i.buyanov@mts.ai>
2023-09-29 15:49:36 +0300

    naming fixed

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,94 +1,83 @@
-const defaultParams = {
-    
-}
 export default class ColumnChart {
-    constructor(cfg){
-        this.link = cfg?.link
-        this.data = cfg?.data
-        this.value = cfg?.value
-        this.label = cfg?.label
-        this.formatHeadig_fn = cfg?.formatHeading
+    constructor(config){
+        this.link = config?.link
+        this.data = config?.data
+        this.value = config?.value
+        this.label = config?.label
+        this.formatHeadig = config?.formatHeading
 
         this.chartHeight = 50
         this.element = document.createElement("div");
         this.render()
     }
 
-    MakeChartBars(arr){
-        let div = document.createElement('div');
+    makeChartBarsElement(dataArr){
+        const divElement = document.createElement('div');
 
-        div.className = "column-chart__chart";
-        div.setAttribute("data-element", "body")
-        const maxValue = Math.max(...arr)
+        divElement.className = "column-chart__chart";
+        divElement.setAttribute("data-element", "body")
+        const maxValue = Math.max(...dataArr)
         const scale = this.chartHeight / maxValue
-        for(let d of arr){
+
+        for(let val of dataArr){
             let el = document.createElement("div")
-            el.setAttribute("style", `--value: ${String(Math.floor(d * scale))}`)
-            el.setAttribute("data-tooltip", `${(d/maxValue*100).toFixed(0)}%`)
-            div.append(el)
+            el.setAttribute("style", `--value: ${String(Math.floor(val * scale))}`)
+            el.setAttribute("data-tooltip", `${(val/maxValue*100).toFixed(0)}%`)
+            divElement.append(el)
         }
-        return div;
+        return divElement;
     }
 
-    MakeChartHeader(){
-        let head_div = document.createElement("div");
-        head_div.className = "column-chart__header"
-        head_div.setAttribute("data-element", "header")
+    makeChartHeaderElement(){
+        const headElement = document.createElement("div");
+        headElement.className = "column-chart__header"
+        headElement.setAttribute("data-element", "header")
 
-        if(this.formatHeadig_fn){
-            head_div.innerHTML = this.formatHeadig_fn(this.value)
-        } else {
-            head_div.innerHTML = this.value
-        }
-        return head_div;
+        headElement.innerHTML = this.formatHeadig ? this.formatHeadig(this.value) : this.value;
+        return headElement;
     }
 
-    MakeChartContainer(){
-        let container_div = document.createElement("div");
-        container_div.append(this.MakeChartHeader())
-        container_div.className = "column-chart__container";
+    makeChartContainerElement(){
+        const containerElement = document.createElement("div");
+        containerElement.append(this.makeChartHeaderElement())
+        containerElement.className = "column-chart__container";
 
         if(this.data){
             if(this.data.length > 0){
-                container_div.append(this.MakeChartBars(this.data))
+                containerElement.append(this.makeChartBarsElement(this.data))
                 this.element.classList.remove("column-chart_loading")
-                return container_div
+                return containerElement
             }
         }
-        return container_div
+        return containerElement
     }
 
-    update(new_arr){
+    update(newDataArr){
         this.element.classList.remove("column-chart_loading")
-        let container_div = this.element.getElementsByClassName("column-chart__container")[0]
-        let chart_div = this.element.getElementsByClassName("column-chart__chart")[0]
+        const container_div = this.element.getElementsByClassName("column-chart__container")[0]
+        const chart_div = this.element.getElementsByClassName("column-chart__chart")[0]
         chart_div.remove()
-        container_div.append(this.MakeChartBars(new_arr))
+        container_div.append(this.makeChartBarsElement(newDataArr))
     }
 
     render(){
         this.element.classList.add("column-chart", "column-chart_loading")
 
-        let title_div = document.createElement("div")
-        title_div.className = "column-chart__title"
-        if (this.label){
-            title_div.innerHTML = `Total ${this.label}`
-        } else {
-            title_div.innerHTML = "Unknown"
-        }
+        const titleElement = document.createElement("div")
+        titleElement.className = "column-chart__title"
+        titleElement.innerHTML = this.label ? `Total ${this.label}` : "Unknown";
 
         if(this.link)
         {
-            let link = document.createElement("a")
+            const link = document.createElement("a")
             link.className = "column-chart__link"
             link.href = this.link
             link.innerHTML = "View all"
-            title_div.append(link)
+            titleElement.append(link)
         }
 
-        this.element.append(title_div)
-        
-        this.element.append(this.MakeChartContainer())
+        this.element.append(titleElement)
+        this.element.append(this.makeChartContainerElement())
     }
 
     destroy(){
