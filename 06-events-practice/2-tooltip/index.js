031e4b5ecf3020af6605e0dda35ae8e59d540add
Igor Buyanov <i.buyanov@mts.ai>
2023-10-08 17:00:54 +0300

    added 6.2 and 6.3

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,50 @@
 class Tooltip {
+  static instance;
+
+  constructor() {
+    if(Tooltip.instance){
+     return Tooltip.instance
+    }
+    Tooltip.instance = this;
+    this.bodyElement = document.getElementsByTagName("body")[0]
+    
+  }
+
+  pointeroverHandler = e =>{
+    if (e.target.dataset.tooltip != undefined) {
+      this.render(e.target.dataset.tooltip)
+    }
+  }
+
+  pointeroutHander = e => {
+    if (e.target.dataset.tooltip != undefined) {
+      this.remove()
+    }
+  }
+
+  render(str){
+    this.element = document.createElement("div")
+    this.element.innerHTML = str;
+    this.element.className = "tooltip"
+    this.bodyElement.append(this.element)
+
+  }
+
+  remove(){
+    this.element.remove()
+  }
+
   initialize () {
+    
+    this.bodyElement.addEventListener("pointerover", this.pointeroverHandler)
+    this.bodyElement.addEventListener("pointerout", this.pointeroutHander)
+
+  }
+
+  destroy(){
+    this.element.remove()
+    this.bodyElement.removeEventListener("pointerover", this.pointeroverHandler)
+    this.bodyElement.removeEventListener("pointerout", this.pointeroutHander)
 
   }
 }
--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,3 +1,90 @@
+function createElementC(tag_name, class_info=undefined, attrs=undefined){
+    const element = document.createElement(tag_name)
+    if(class_info)
+    {
+      if(class_info instanceof Array){
+        element.classList.add(...class_info);
+      } else{
+        element.className = class_info;
+      }
+    }
+  
+    if(attrs){
+      for (const [key, value] of Object.entries(attrs)) {
+        element.setAttribute(key, value)
+      }
+    }
+    return element
+  }
+  
+  function templateToElement(template){
+    const element = document.createElement("div");
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
 export default class DoubleSlider {
+
+    constructor(config){
+        this.config = config
+        this.element;
+        this.isPointerPressed = false;
+        this.render();
+        this.element.addEventListener("pointermove", this.movementHandler)
+        this.element.addEventListener("pointerdown", this.pointerdownHandler)
+        this.element.addEventListener("pointerup", this.pointerupHandler)
+    }
+
+    sliderTemplate(data){
+        let min, max;
+        if(data.selected){
+            min = data.formatValue(data.selected.from)
+            max = data.formatValue(data.selected.to)
+        } else {
+            min = data.formatValue(data.min)
+            max = data.formatValue(data.max)
+        }
+        return `
+        <div class="range-slider">
+            <span data-element="from">${min}</span>
+            <div class="range-slider__inner">
+            <span class="range-slider__progress"></span>
+            <span class="range-slider__thumb-left"></span>
+            <span class="range-slider__thumb-right"></span>
+            </div>
+            <span data-element="to">${max}</span>
+        </div>
+        `
+    }
+
+    movementHandler = e =>
+    {
+        if(this.isPointerPressed)
+        {
+            console.log(e.clientX)
+        }
+    }
+
+    pointerdownHandler = e =>
+    {
+        this.isPointerPressed = true;
+    }
+
+    pointerupHandler = e =>
+    {
+        this.isPointerPressed = false;
+    }
+
+    render(){
+        this.element = templateToElement(this.sliderTemplate(this.config))
+    }
+
+    remove(){
+        this.element.remove()
+    }
+
+    destroy(){
+        this.remove()
+    }
 
 }
